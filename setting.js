d9e7856ef604cfe44e70610f805cfbf48ad0096e
xphonghuhu <xphong.dmca@gmail.com>
2024-03-15 21:35:19 +0700

    first commit

---
+++ setting.js
@@ -0,0 +1,103 @@
+export const userInfo = [
+
+
+    {
+        background: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/3cfd178b-66c3-4756-8e9b-63df1ff3e510/dfe2mmj-2780d3da-7a83-4d39-9d29-3ab8cb3d3f6f.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzNjZmQxNzhiLTY2YzMtNDc1Ni04ZTliLTYzZGYxZmYzZTUxMFwvZGZlMm1tai0yNzgwZDNkYS03YTgzLTRkMzktOWQyOS0zYWI4Y2IzZDNmNmYuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.JFvZ1fk9SwWcQSAQwNY_3cE-RiD96bDljFE3N0AaRwA",
+        avatar: 'https://raw.githubusercontent.com/levinhkhangzz/assets/main/IMG_3116.JPG',
+        name: 'Lê Vĩnh Khang (Aedotris)',
+        info: 'I am a guy passionate about software creativity.',
+
+    }
+
+
+
+];
+export const secctionCopy = [
+
+
+    {
+        image: "https://th.bing.com/th/id/R.a569763f29c9cc20b167d4c47e9b95fd?rik=0VUzdeUXmeT7OA&riu=http%3a%2f%2fis1.mzstatic.com%2fimage%2fthumb%2fPurple127%2fv4%2fba%2fa3%2fec%2fbaa3ec06-4110-43db-0fe3-5d728002a2b0%2fsource%2f1200x630bb.jpg&ehk=VO43hY4xqXmzYPiczcKkYpPvwDKj4%2ftG4f8iLd2UBII%3d&risl=&pid=ImgRaw&r=0",
+        title: 'Instagram',
+        name: 'williamcachamwri',
+        link: 'https://instagram.com/williamcachamwri',
+
+    },
+    {
+        image: "https://levinhkhang.org/_next/static/img/GITHUB.webp",
+        title: 'Github',
+        name: 'levinhkhangzz',
+        link: 'https://github.com/levinhkhangzz',
+
+    },
+    {
+        image: "https://th.bing.com/th/id/OIP.Fmbl3XZiHagmGL9tKx0kUAHaHa?rs=1&pid=ImgDetMain",
+        title: 'Telegram',
+        name: 'A3d0tr1sX',
+        link: 'https://t.me/A3d0tr1sX',
+
+    },
+    {
+        image: "https://is1-ssl.mzstatic.com/image/thumb/Purple116/v4/ad/dd/75/addd7565-7b92-14cf-3911-3cd328e13c78/AppIcon-0-0-1x_U007emarketing-0-10-0-0-85-220.png/512x512bb.jpg",
+        title: 'Gemini Pro Chat',
+        name: 'Chat',
+        link: 'https://chat.levinhkhang.org/',
+
+    },
+    {
+        image: "https://th.bing.com/th/id/OIP.Xo_FCK4zl3xBtFXByduYzQHaHa?rs=1&pid=ImgDetMain",
+        title: 'Facebook',
+        name: 'Lê Vĩnh Khang',
+        link: 'https://www.facebook.com/profile.php?id=199717642',
+
+    },
+    {
+        image: "https://i.pinimg.com/736x/1d/07/c2/1d07c2bacaefc123ddc782e6288ddc41.jpg",
+        title: 'Zalo',
+        name: 'Lê Vĩnh Khang',
+        link: 'https://zalo.me/levinhkhangdeptrai',
+
+    },
+
+
+];
+
+export const modalssss = [
+
+
+    {
+        image: "https://th.bing.com/th/id/R.a60b690a4f78161e558c959e157d7b0d?rik=0cISg63VqH72cg&pid=ImgRaw&r=0",
+        title: 'MB Bank',
+        name: 'Get QR Code',
+        modal: 'https://img.vietqr.io/image/MB-0905243477-print.png',
+
+
+    },
+    {
+        image: "https://laserbinhthuan.com/wp-content/uploads/2021/10/meta-logo-facebook.jpg",
+        title: 'Certificate',
+        name: 'Meta',
+        modal: 'https://raw.githubusercontent.com/levinhkhangzz/assets/main/z5164475138499_f66102b26d26a8b2e05af624fffab1e0.jpg',
+
+
+    },
+    {
+        image: "https://th.bing.com/th/id/OIP.kY7h_2SMSJv6fdNrPrAROQAAAA?w=400&h=400&rs=1&pid=ImgDetMain",
+        title: 'Certificate',
+        name: 'Google Cloud',
+        modal: 'https://raw.githubusercontent.com/levinhkhangzz/assets/main/z5164475157110_f11989f1964f1fb581f7d2efd0bb5788.jpg',
+
+
+    },
+    {
+        image: "https://th.bing.com/th/id/OIP.ysdCJiI4OEy9VF-5ituvWwAAAA?w=300&h=300&rs=1&pid=ImgDetMain",
+        title: 'Certificate',
+        name: 'Amazon Web Service',
+        modal: 'https://raw.githubusercontent.com/levinhkhangzz/assets/main/z5164475154720_a0157710d39eed70389cf7b43ff6793a.jpg',
+
+
+    },
+
+
+
+
+];
